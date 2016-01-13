21df7ad99d60eb444513f2a8eaccf59e2aef00e6
Matthew Newell and brooke hurford <student@epicodus.com>
2016-01-13 09:17:07 -0800

    Conditionals with numbers and else if.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var question = parseInt(prompt("How tall are you in inches?"));
+
+  if(question >= 43) {
+    $('#rides').show();
+} else {
+    $('#under-43').show();
+  }
+});
